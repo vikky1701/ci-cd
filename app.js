f0a02031ed8fc022bbfb4f34c54dc418f86ab0ba
vikky <vy945530@gmail.com>
2025-07-08 14:32:23 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello, CI/CD World!");
+});
+
+const PORT = process.env.PORT || 3000;
+if (require.main === module) {
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+}
+
+module.exports = app;
